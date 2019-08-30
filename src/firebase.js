f96b7f5790ec5778b59d79a8458e1e6b0ef7ab57
Ralphie Celedon <ralphie@Ralphies-MacBook-Pro.local>
2019-08-30 14:31:39 -0700

    created databases

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = firebase.initialize({
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messageSenderId: "",
-    apiId: "",
+    apiKey: "AIzaSyA0yGUH6rxmN-1qWpwTIrQnf_3tMNwaYp0",
+    authDomain: "todoist-clone-4fc2a.firebaseapp.com",
+    databaseURL: "https://todoist-clone-4fc2a.firebaseio.com",
+    projectId: "todoist-clone-4fc2a",
+    storageBucket: "todoist-clone-4fc2a.appspot.com",
+    messagingSenderId: "347177863128",
+    appId: "1:347177863128:web:7a8b8f9899aa5295"
 });
 
 export { firebaseConfig as firebase };
