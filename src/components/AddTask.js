405cac910ad5ec5036bd51c421e1cfbb0147c08c
Ralphie Celedon <ralphie@ralphies-mbp.attlocal.net>
2019-09-02 23:17:22 -0700

    built out add task component

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -4,7 +4,7 @@ import moment from "moment";
 import { firebase } from"../firebase";
 import { useSelectedProjectValue } from "../context";
 
-export const AddTask = ({ showAddTaskMain = true, showShouldMain = false, showQuickAddTask, setShowQuickAddTask, }) => {
+export const AddTask = ({ showAddTaskMain = true, shouldShowMain = false, showQuickAddTask, setShowQuickAddTask, }) => {
     const [task, setTask] = useState("");
     const [taskDate, setTaskDate] = useState("");
     const [project, setProject] = useState("");
@@ -15,9 +15,55 @@ export const AddTask = ({ showAddTaskMain = true, showShouldMain = false, showQu
     const { selectedProject } = useSelectedProjectValue();
 
     const addTask = () => {
-        const projectId = project || selectedProject
-    }
+        const projectId = project || selectedProject;
+        let collatedDate = "";
+
+        if (projectId === "TODAY") {
+            collatedDate = moment().format("MM/DD/YYYY");
+        } else if (projectId === "NEXT_7") {
+            collatedDate = moment()
+            .add(7, "days")
+            .format("MM/DD/YYYY");
+        }
+
+        return (
+            task && 
+            projectId && 
+            firebase
+                .firestore()
+                .collection("tasks")
+                .add({
+                    archived: false,
+                    projectId,
+                    task,
+                    date: collatedDate || taskDate,
+                    userId: "basudbHBSjksfb38csudh"
+                })
+                .then(() => {
+                    setTask("");
+                    setProject("");
+                    setShowMain("");
+                    setShowProjectOverlay(false);
+                })
+        );
+    };
 
 
-    return ();
+    return (
+        <div 
+            className={showQuickAddTask ? "add-task add-task__overlay" : "add-task"}
+            data-testid="add-task-comp"
+        >
+            {showAddTaskMain && (
+                <div
+                    className="add-task__shallow"
+                    data-testid="show-main-action"
+                    onClick={() => setShowMain(!showMain)}
+                >
+                    <span className="add-task__plus">+</span>
+                    <span className="add-task__text">Add Task</span>
+                </div>
+            )}
+        </div>
+    );
 };
